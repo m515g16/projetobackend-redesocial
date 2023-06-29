39f0ee6535e8676c35613ca618db0ba2ff4ce7e7
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-06-29 17:13:29 -0300

    feat: add libs, create user's model-serializer-views-urls

--- rede_social/urls.py
+++ rede_social/urls.py
@@ -25,6 +25,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include("usuarios.urls")),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
