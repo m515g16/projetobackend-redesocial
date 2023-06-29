339839529ed9b9c21e58093da5a207a6bdf5e0e7
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-06-29 16:59:36 -0300

    add libs, create user's views-model-serializer-urls-permission,

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,10 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    
+    path("usuarios/create/", views.CreateUsuario.as_view()),
+    path("usuarios/list/", views.RetrieveUsuario.as_view()),
+    path("usuarios/update/", views.UpdateUsuario.as_view()),
+    path("usuarios/delete/", views.DestroyUsuario.as_view()),
     path("usuarios/login/", jwt_views.TokenObtainPairView.as_view()),
     path("usuarios/refresh/", jwt_views.TokenRefreshView.as_view()),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Usuario
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UsuarioSerializer
+from django.shortcuts import get_object_or_404
+from .permissions import IsAccountOwner
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+
+class CreateUsuario(CreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class RetrieveUsuario(RetrieveAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    lookup_url_kwarg = "pk"
+
+class UpdateUsuario(UpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    lookup_url_kwarg = "pk"
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=partial,
+        )
+        serializer.is_valid(raise_exception=True)
+
+        if 'password' in request.data:
+            password = request.data['password']
+            instance.set_password(password)
+
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+class DestroyUsuario(DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    lookup_url_kwarg = "pk"
+
