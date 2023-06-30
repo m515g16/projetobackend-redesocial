6cc068a63285f5b7b66c798ea525894e1a4506a1
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-06-30 16:23:53 -0300

    fix: models and relationships

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,17 +5,22 @@ class User(AbstractUser):
     class Meta:
         ordering = ["name"]
 
-    email = models.CharField(max_length=127, unique=True)
-    name = models.CharField(max_length=127)
-    birthdate = models.DateField()
+    email = models.CharField(max_length=120, unique=True)
+    name = models.CharField(max_length=60)
+    birthdate = models.DateField(null=True)
+    perfil = models.CharField(max_length=180, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    follower_friend = models.ManyToManyField(
-        "usuarios.User", through="usuarios.FollowerFriends", related_name="seguidores"
+    followers = models.ManyToManyField(
+        "usuarios.User", through="usuarios.Follower", related_name="seguidores"
     )
 
-class FollowerFriends(models.Model):
+    friends = models.ManyToManyField(
+        "usuarios.User", through="usuarios.Friend", related_name="amigos"
+    )
+
+class Follower(models.Model):
     usuario = models.ForeignKey(
         "usuarios.User", on_delete=models.CASCADE, related_name="followed_user"
     )
@@ -24,6 +29,15 @@ class FollowerFriends(models.Model):
         "usuarios.User", on_delete=models.CASCADE, related_name="follower_user"
     )
 
-    amigo = models.BooleanField(null=True, default=False)
-    
-    
+class Friend(models.Model):
+    usuario = models.ForeignKey(
+        "usuarios.User", on_delete=models.CASCADE, related_name="user"
+    )
+
+    friend = models.ForeignKey(
+        "usuarios.User", on_delete=models.CASCADE, related_name="friend"
+    )
+
+    pendding = models.BooleanField(default=True)
+    accepted = models.BooleanField(default=False)
+    solicited = models.BooleanField(default=True) 
