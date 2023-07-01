9cb80231b8bc9f69e521983c9c0e8462d08fd6e7
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-07-01 14:37:50 -0300

    feat: start follow's logic

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("usuarios/list/", views.ListUsuario.as_view()),
     path("usuarios/create/", views.CreateUsuario.as_view()),
-    path("usuarios/retrieve/<int:pk>/", views.RetrieveUsuario.as_view()),
-    path("usuarios/update/<int:pk>/", views.UpdateUsuario.as_view()),
-    path("usuarios/delete/<int:pk>/", views.DestroyUsuario.as_view()),
+    path("usuarios/ret-upd-des/<int:pk>/", views.RetrieveUpdateDestroyUsuario.as_view()),
+    path("usuarios/follow/", views.FollowUsuario.as_view()),
+    path("usuarios/friend-request/", views.RetrieveUpdateDestroyUsuario.as_view()),
     path("usuarios/login/", jwt_views.TokenObtainPairView.as_view()),
     path("usuarios/refresh/", jwt_views.TokenRefreshView.as_view()),
 ]
