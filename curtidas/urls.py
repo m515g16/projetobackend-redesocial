2bfa7f1968ca0c7b5531225d81ebe85a9fd2c141
Ranier_Dias <ranierfernandodias@gmail.com>
2023-07-04 15:50:13 -0300

    feat: add routers to like

--- curtidas/urls.py
+++ curtidas/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import LikeView
+from .views import LikeCreateView, LikeDestroyView, LikeListView, LikeRetrieveUserView
 
 urlpatterns = [
-    path("", LikeView.as_view()),
+    path("", LikeCreateView.as_view()),
+    path("<int:pk>/", LikeDestroyView.as_view()),
+    path("publication/<int:publication_id>/", LikeListView.as_view()),
+    path("publication/<int:publication_id>/user/",
+         LikeRetrieveUserView.as_view())
 ]
