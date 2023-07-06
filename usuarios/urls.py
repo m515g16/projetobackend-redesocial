e15c649a771b7a59d7ca621f4c38a368b033e09f
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-07-06 16:54:43 -0300

    terminado, só revisando

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,17 +4,17 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     
-    path("usuarios/", views.ListCreateUsuario.as_view()),
-    path("usuarios/<int:pk>/",
+    path("", views.ListCreateUsuario.as_view()),
+    path("<int:pk>/",
          views.RetrieveUpdateDestroyUsuario.as_view()),
 
-    path("usuarios/follow/list/", views.ListFollow.as_view()),
-    path("usuarios/follow/", views.FollowUsuario.as_view()),
-    path("usuarios/follow/<int:pk>/", views.DeleteFollowUsuario.as_view()),
+    path("follow/list/", views.ListFollow.as_view()),
+    path("follow/", views.FollowUsuario.as_view()),
+    path("follow/<int:pk>/", views.DeleteFollowUsuario.as_view()),
 
-    path("usuarios/friend/", views.FriendUsuario.as_view()),
-    path("usuarios/friend/<int:pk>/", views.RetriveUpdateDestroyFriendUsuario.as_view()),
+    path("friend/", views.FriendUsuario.as_view()),
+    path("friend/<int:pk>/", views.RetriveUpdateDestroyFriendUsuario.as_view()),
 
-    path("usuarios/login/", jwt_views.TokenObtainPairView.as_view()),
-    path("usuarios/refresh/", jwt_views.TokenRefreshView.as_view()),
+    path("login/", jwt_views.TokenObtainPairView.as_view()),
+    path("refresh/", jwt_views.TokenRefreshView.as_view()),
 ]
