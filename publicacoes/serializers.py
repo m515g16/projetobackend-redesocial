5181f67b10f4bf21401b5a545e7796e1b8916e93
Ranier Dias <110170807+RanierDias@users.noreply.github.com>
2023-07-10 17:58:13 +0000

    feat: add like and comment to router publication

--- publicacoes/serializers.py
+++ publicacoes/serializers.py
@@ -16,9 +16,24 @@ class PublicationSerializer(serializers.ModelSerializer):
 
 
 class PublicationUserSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+
+    def get_likes(self, obj):
+        likes_publication = Like.objects.filter(publication=obj)
+        serializer = LikeSerializer(likes_publication, many=True)
+
+        return serializer.data
+
+    def get_comments(self, obj):
+        comments_publication = Comment.objects.filter(publication=obj)
+        serializer = CommentSerializer(comments_publication, many=True)
+
+        return serializer.data
+
     class Meta:
         model = Publication
-        fields = ("id", "image", "text", "public", "created_at")
+        fields = ("id", "image", "text", "public", "likes", "comments", "created_at")
 
 
 class PublicationTimeLineSerializer(serializers.ModelSerializer):
