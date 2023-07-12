2c4545113e09150d9394753d167f9d1f74899e19
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-07-12 08:43:21 -0300

    add others models in django admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Followers, FriendSolicitations
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Followers)
+admin.site.register(FriendSolicitations)
